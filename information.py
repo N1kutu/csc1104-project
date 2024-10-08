1854b3a2f69d58c611911b027fd2ad3dc4b36bb8
Nik Shautsou <95721742+N1kutu@users.noreply.github.com>
2024-10-08 14:53:37 +0100

    work from second zoom

--- information.py
+++ information.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib
-matplotlib.use('TkAgg') 
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Updated_Test.csv')
+df = pd.read_csv("Data.csv")
 
 print(df.describe())
-print(df['Cores'].hist())
+print(df["Cores"].hist())
 
-'''
+"""
 x = df["Cores"]
 y = df["CPU Name"]
 plt.scatter(x, y, label= "stars", color= "m",  
             marker= "*", s=30) 
-'''
+"""
 
 plt.show()
