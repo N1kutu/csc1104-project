dda574dbaad0af753728b304756fde6022d62bfd
Nik Shautsou <nik0101@icloud.com>
2024-10-13 19:53:12 +0000

    modified information.py to include graphs of our data

--- information.py
+++ information.py
@@ -5,21 +5,15 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("Data.csv")
-
-print(df.describe())
+df2 = pd.read_csv("Data_trimmed10%.csv")
 # print(df.mean())
-# print(df["Cores"].hist())
+
+# print(df["Cores"].hist(alpha=0.9))
+print(df2["Cores"].hist(alpha=0.9))
 meanCpu = df.loc[
     (df["No. Sockets"] == 1) & (df["Cores"] == 8) & (df["CPU Mark"] == 8672)
 ]
 
-meanCpu.to_csv("MeanCPU.csv")
+# meanCpu.to_csv("MeanCPU.csv")
 
-"""
-x = df["Cores"]
-y = df["CPU Name"]
-plt.scatter(x, y, label= "stars", color= "m",  
-            marker= "*", s=30) 
-"""
-
-# plt.show()
+plt.show()
